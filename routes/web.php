11ac9c89f1349bf3117279145332068d074c5c1a
blotalt <baska7gzihkara71@gmail.com>
2026-07-07 23:25:38 +0700

    feature1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth', 'role:admin'])->get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+
+Route::middleware(['auth', 'role:teacher'])->get('/teacher/dashboard', function () {
+    return view('teacher.dashboard');
+})->name('teacher.dashboard');
+
+Route::middleware(['auth', 'role:student'])->get('/student/dashboard', function () {
+    return view('student.dashboard');
+})->name('student.dashboard');
